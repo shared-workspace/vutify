6e27dc39ee303197b14c6e4d0695e9594184c6cc
ADARSH SAJWAN <ADARSHSAJWAN.210111034@gehu.ac.in>
2024-12-06 00:50:40 +0000

    vutify setup

---
+++ src/pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <!-- <HelloWorld /> -->
+  <v-sheet
+    color="info"
+    height="26rem"
+  >
+    <v-row>
+      <v-col>
+        <h1>Heading</h1>
+        <h3>Sub Heading</h3>
+        <v-btn color="info">
+          Try For Free
+        </v-btn>
+      </v-col>
+    </v-row>
+  </v-sheet>
+</template>
+
+<script lang="ts" setup>
+//
+</script>
